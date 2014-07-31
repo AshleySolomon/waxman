97b8065fd1ea9f9e85eb6513ef94ce03410c6e54
AshleySolomon <ashley.solomon@loop.colum.edu>
2014-07-31 15:08:50 -0500

    Finished Burn Time filter layout @320
    Also progress on Burn Time filter layout @480

--- php/filters-menu.php
+++ php/filters-menu.php
@@ -2,14 +2,45 @@
     <div class="category">
         BURN TIME
     </div>
-    <div>
-        <ul class="sub-category">
+    <div class="sub-category">
+        <form>
+            <div class="check set-width" title="1-10 Hours">
+                <input type="checkbox" name="burn-time" value="burn1">
+                    <img src="../images/big-flame.svg" alt="flame1">
+                    <p>1-10 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="10-20 Hours">
+                <input type="checkbox" name="burn-time" value="burn2">
+                    <img src="../images/flame2.svg" alt="flame2">
+                    <p>10-20 Hours</p>
+                    <br>
+            </div>
+            <div class="check set-width" title="20-60 Hours">
+                <input type="checkbox" name="burn-time" value="burn3">
+                    <img src="../images/flame3.svg" alt="flame3">
+                    <p>20-60 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="60-100 Hours">
+                <input type="checkbox" name="burn-time" value="burn4">
+                    <img src="../images/flame4.svg" alt="flame4">
+                    <p>60-100 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="100+ Hours">
+                <input type="checkbox" name="burn-time" value="burn5">
+                    <img src="../images/flame5.svg" alt="flame5">
+                    <p>100+ Hours</p>
+            </div>
+        </form>
+        <!-- <ul class="sub-category">
             <li>Level 1</li>
             <li>Level 2</li>
             <li>Level 3</li>
             <li>Level 4</li>
             <li>Level 5</li>
-        </ul>
+        </ul> -->
     </div>
     <div class="category">
         DIAMETER
