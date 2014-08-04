19978ff9d41d7f7bab9d60227ee90a55bc32f311
AshleySolomon <ashley.solomon@loop.colum.edu>
2014-08-04 11:25:31 -0500

    Flames, Burn Time and Diameter
    Made flames in Ai
    Added flames as icon keys for Burn Time filter option.
    Stylized choices for Diameter filter option.

--- php/filters-menu.php
+++ php/filters-menu.php
@@ -2,20 +2,89 @@
     <div class="category">
         BURN TIME
     </div>
-    <div>
-        <ul class="sub-category">
-            <li>Level 1</li>
-            <li>Level 2</li>
-            <li>Level 3</li>
-            <li>Level 4</li>
-            <li>Level 5</li>
-        </ul>
+    <div class="sub-category">
+        <form>
+            <div class="check set-width" title="1-10 Hours">
+                <input type="checkbox" name="burn-time" value="burn1">
+                    <img src="../images/big-flame.svg" alt="flame1">
+                    <p>1-10 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="10-20 Hours">
+                <input type="checkbox" name="burn-time" value="burn2">
+                    <img src="../images/flame2.svg" alt="flame2">
+                    <p>10-20 Hours</p>
+                    <br>
+            </div>
+            <div class="check set-width" title="20-60 Hours">
+                <input type="checkbox" name="burn-time" value="burn3">
+                    <img src="../images/flame3.svg" alt="flame3">
+                    <p>20-60 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="60-100 Hours">
+                <input type="checkbox" name="burn-time" value="burn4">
+                    <img src="../images/flame4.svg" alt="flame4">
+                    <p>60-100 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="100+ Hours">
+                <input type="checkbox" name="burn-time" value="burn5">
+                    <img src="../images/flame5.svg" alt="flame5">
+                    <p>100+ Hours</p>
+            </div>
+        </form>        
     </div>
     <div class="category">
         DIAMETER
     </div>
-    <div>
-        <ul class="sub-category">
+    <div class="sub-category">
+        <form>
+            <div class="left"> 
+                <div class="check diameter">           
+                    <input type="checkbox" name="diameter" value="7/8">
+                        <p>7/8"</p>
+                        <br>      
+                </div> 
+                <div class="check diameter">    
+                    <input type="checkbox" name="diameter" value="1/2">
+                        <p>1/2"</p>
+                        <br>
+                </div>
+                <div class="check diameter">
+                    <input type="checkbox" name="diameter" value="2">
+                        <p>2"</p>
+                        <br>
+                </div>
+                <div class="check diameter">
+                    <input type="checkbox" name="diameter" value="2.5">
+                        <p>2.5"</p>
+                        <br>
+                </div>
+            </div>            
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="3">
+                    <p>3"</p>
+                    <br>
+            </div>
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="4">
+                    <p>4"</p>
+                    <br>
+            </div>
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="5">
+                    <p>5"</p>
+                    <br>
+            </div>
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="6">
+                    <p>6"</p>
+                    <br>
+            </div>
+           
+        </form>        
+        <!-- <ul class="sub-category">
             <li>7/8"</li>
             <li>1/2"</li>
             <li>2"</li>
@@ -24,7 +93,7 @@
             <li>4"</li>
             <li>5"</li>
             <li>6"</li>
-        </ul>
+        </ul> -->
     </div>
     <div class="category">
         FRAGRANCE
@@ -44,7 +113,7 @@
         </ul>
     </div>
     <div class="category">
-        FRANGRANCE STRENGTH
+        STRENGTH
     </div>
     <div>
         <ul class="sub-category">
