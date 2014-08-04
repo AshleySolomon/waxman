325fc7bbaa4b6f961af0d430c01f1b1b1895e726
AshleySolomon <ashley.solomon@loop.colum.edu>
2014-08-04 12:01:36 -0500

    Fragrance, Strength, Product Mat. filters
    -Changed font to 13px and normal weight on Fragrance & Strength filter
    options for consistency.
    -Added checkboxes to Product Material filter options

--- php/filters-menu.php
+++ php/filters-menu.php
@@ -2,29 +2,88 @@
     <div class="category">
         BURN TIME
     </div>
-    <div>
-        <ul class="sub-category">
-            <li>Level 1</li>
-            <li>Level 2</li>
-            <li>Level 3</li>
-            <li>Level 4</li>
-            <li>Level 5</li>
-        </ul>
+    <div class="sub-category">
+        <form id="burn-time">
+            <div class="check set-width" title="1-10 Hours">
+                <input type="checkbox" name="burn-time" value="burn1">
+                    <img src="../images/big-flame.svg" alt="flame1">
+                    <p>1-10 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="10-20 Hours">
+                <input type="checkbox" name="burn-time" value="burn2">
+                    <img src="../images/flame2.svg" alt="flame2">
+                    <p>10-20 Hours</p>
+                    <br>
+            </div>
+            <div class="check set-width" title="20-60 Hours">
+                <input type="checkbox" name="burn-time" value="burn3">
+                    <img src="../images/flame3.svg" alt="flame3">
+                    <p>20-60 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="60-100 Hours">
+                <input type="checkbox" name="burn-time" value="burn4">
+                    <img src="../images/flame4.svg" alt="flame4">
+                    <p>60-100 Hours</p>
+                    <br>
+            </div>
+            <div class="check" title="100+ Hours">
+                <input type="checkbox" name="burn-time" value="burn5">
+                    <img src="../images/flame5.svg" alt="flame5">
+                    <p>100+ Hours</p>
+            </div>
+        </form>        
     </div>
     <div class="category">
         DIAMETER
     </div>
-    <div>
-        <ul class="sub-category">
-            <li>7/8"</li>
-            <li>1/2"</li>
-            <li>2"</li>
-            <li>2.5"</li>
-            <li>3"</li>
-            <li>4"</li>
-            <li>5"</li>
-            <li>6"</li>
-        </ul>
+    <div class="sub-category">
+        <form id="diameter">
+            <div class="left"> 
+                <div class="check diameter">           
+                    <input type="checkbox" name="diameter" value="7/8">
+                        <p>7/8"</p>
+                        <br>      
+                </div> 
+                <div class="check diameter">    
+                    <input type="checkbox" name="diameter" value="1/2">
+                        <p>1/2"</p>
+                        <br>
+                </div>
+                <div class="check diameter">
+                    <input type="checkbox" name="diameter" value="2">
+                        <p>2"</p>
+                        <br>
+                </div>
+                <div class="check diameter">
+                    <input type="checkbox" name="diameter" value="2.5">
+                        <p>2.5"</p>
+                        <br>
+                </div>
+            </div>
+
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="3">
+                    <p>3"</p>
+                    <br>
+            </div>
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="4">
+                    <p>4"</p>
+                    <br>
+            </div>
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="5">
+                    <p>5"</p>
+                    <br>
+            </div>
+            <div class="check diameter">
+                <input type="checkbox" name="diameter" value="6">
+                    <p>6"</p>
+                    <br>
+            </div>           
+        </form>        
     </div>
     <div class="category">
         FRAGRANCE
@@ -44,11 +103,11 @@
         </ul>
     </div>
     <div class="category">
-        FRANGRANCE STRENGTH
-    </div>
-    <div>
+        STRENGTH
+    </div>  
+    <div>  
         <ul class="sub-category">
-            <li>SINGLE STENGTH</li>
+            <li>SINGLE STRENGTH</li>
             <li>DOUBLE STRENGTH</li>
             <li>TRIPLE STRENGTH</li>
         </ul>
@@ -64,12 +123,28 @@
     <div class="category">
         PRODUCT MATERIAL
     </div>
-    <div>
-        <ul class="sub-category">
-            <li>BEESWAX</li>
-            <li>PARAFFIN</li>
-            <li>SOY</li>
-            <li>VEGETABLE OIL</li>
-        </ul>
+    <div class="sub-category">
+        <form id="material">      
+            <div class="check">
+                <input type="checkbox" name="material" value="beeswax">
+                    <p>BEESWAX</p>
+                    <br>
+            </div>
+            <div class="check">
+                <input type="checkbox" name="material" value="paraffin">
+                    <p>PARAFFIN</p>
+                    <br>
+            </div>            
+            <div class="check">
+                <input type="checkbox" name="material" value="soy">
+                    <p>SOY</p>
+                    <br>  
+            </div>
+            <div class="check">
+                <input type="checkbox" name="material" value="veggie_oil">
+                    <p>VEGETABLE OIL</p>
+                    <br>  
+            </div>
+        </form>
     </div>                
 </div> 
